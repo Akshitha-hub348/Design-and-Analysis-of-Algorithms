a0b8eced727118f4ee4b28fd09004b8e7b2c0c29
Akshitha chukkapalli <chukkapalliakshitha6788@gmail.com>
2026-09-19 11:57:41 +0530

    Add Prim's algorithm implementation in Prims.c
    Implement Prim's algorithm to find the minimum spanning tree of a graph using an adjacency matrix.

---
+++ Prims.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#define MAX 20
+#define INF 9999
+
+int main()
+{
+    int n, graph[MAX][MAX];
+    int selected[MAX] = {0};
+    int i, j, edges = 0, total = 0;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    selected[0] = 1;
+
+    printf("\nPrim's MST:\n");
+
+    while (edges < n - 1)
+    {
+        int min = INF;
+        int u = -1, v = -1;
+
+        for (i = 0; i < n; i++)
+        {
+            if (selected[i])
+            {
+                for (j = 0; j < n; j++)
+                {
+                    if (!selected[j] && graph[i][j] != 0 &&
+                        graph[i][j] < min)
+                    {
+                        min = graph[i][j];
+                        u = i;
+                        v = j;
+                    }
+                }
+            }
+        }
+
+        printf("%d - %d : %d\n", u, v, min);
+
+        total += min;
+        selected[v] = 1;
+        edges++;
+    }
+
+    printf("Total cost = %d\n", total);
+
+    return 0;
+}
